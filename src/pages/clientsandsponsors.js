c617f3420008f9811eb9038a27177229ced3535d
Mariel Aquino <marielaquino@berkeley.edu>
2018-04-15 22:45:44 -0700

    first commit

--- src/pages/clientsandsponsors.js
+++ src/pages/clientsandsponsors.js
@@ -5,6 +5,7 @@ const clientsandsponsors = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
