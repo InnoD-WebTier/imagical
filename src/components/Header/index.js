e67b925c6eb6df92428d4181a70317f01817492f
trev4ev <trevoraquino@gmail.com>
2018-04-17 18:28:38 -0700

    convert header to bootstrap

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,20 +3,22 @@ import Link from 'gatsby-link'
 import logo from '../../images/imagi_logo.png'
 
 const Header = () => (
-    <div className="header">
-        <div className="header__left-items">
-            <Link to="/">
-                <img src={logo} alt="logo" className="header__logo" />
-            </Link>
-        </div>
-        <div className="header__right-items">
-            <ul className="header__routing-links">
+    <nav className="navbar navbar-expand-lg navbar-custom">
+        <Link to="/">
+            <img src={logo} alt="logo" className="header__logo" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto header__routing-links">
                 <li className="header__route">
                     <Link to="/contact" activeClassName="header__link--active" className="header__link">contact</Link>
                 </li>
                 <li className="header__route">
                     <Link to="/apply" activeClassName="header__link--active" className="header__link">apply</Link>
-                </li>
+            </li>
                 <li className="header__route">
                     <Link to="/partners" activeClassName="header__link--active" className="header__link">clients & sponsors</Link>
                 </li>
@@ -28,8 +30,7 @@ const Header = () => (
                 </li>
             </ul>
         </div>
-        <div style={{clear: 'both'}}> </div>
-    </div>
+    </nav>
 )
 
 export default Header
