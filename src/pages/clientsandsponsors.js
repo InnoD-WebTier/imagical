efed083977ef696d56ad1c8c5467be892f9fd537
Mariel Aquino <marielaquino@berkeley.edu>
2018-04-20 14:43:33 -0700

    button component

--- src/pages/clientsandsponsors.js
+++ src/pages/clientsandsponsors.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
+import '../styles/index.css'
+ 
+const image = '../images/MissionU.png'
 
-const clientsandsponsors = () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
-)
 
-export default clientsandsponsors
+export default class LightboxClient extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <div>
+        <h1 className="title">CLIENTS</h1>
+        <button type='button' onClick={() => this.setState({ isOpen: true})}>
+          MissionU
+        </button>
+
+        {isOpen &&
+          <Lightbox
+            mainSrc = {image}
+            onCloseRequest = {() => this.setState({ isOpen: false })}
+            />
+          }
+        </div>
+        );
+  }
+}
+
+
