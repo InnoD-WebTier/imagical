fb0b8ba2063b2e7ec3a69ad28a42a6d04ae50c3c
Mariel Aquino <marielaquino@berkeley.edu>
2018-04-22 18:21:27 -0700

    adjusted photo size

--- src/pages/clientsandsponsors.js
+++ src/pages/clientsandsponsors.js
@@ -21,7 +21,7 @@ export default class LightboxClient extends Component {
       <div class='imgContainer'>
         <h1 className="title">CLIENTS</h1>
             <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPy7ZnI46zizXRVoXLbV4Gl6LamFU6g5pqtJj1ijxnO8i-gJWp" width="100px" height="100px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPy7ZnI46zizXRVoXLbV4Gl6LamFU6g5pqtJj1ijxnO8i-gJWp" width="150" height="150"/>
             </div>
         <div class="imgButton">
         <button type='button' onClick={() => this.setState({ isOpen: true})}>
