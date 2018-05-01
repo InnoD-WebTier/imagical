a18a4acf4809407a846424c15aa97df7b2e00182
Franco <christopher.franco.m@gmail.com>
2018-04-30 20:53:13 -0700

    flex box is the way

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -9,16 +9,18 @@ export default class Team extends React.Component {
       ));
         return (
           <div className="container">
-            <Image src={this.props.imgSrc} responsive />
-            <div className="row">
-              <div className="btn-position col-6">
+            <div className="img--team-sizing">
+              <Image src={this.props.imgSrc} responsive />
+            </div>
+            <div className="row--resize">
+              <div className="btn-position col-lg-6">
                 <div>
                     <h2 className="column-title-team">{this.props.teamname}</h2>
                     <p>{this.props.description}</p>
                 </div>
                 <a className="btn-apply" href="#" type="button"> Apply</a>
               </div>
-              <div className="col-6">
+              <div className="col-lg-6">
                 <div>
                   <h2 className="column-title-team">Key Deliverables</h2>
                   <p>{this.props.description}</p>
