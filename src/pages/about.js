75eceaeac28e05abf1f09e9a690017dfdaafe12a
Franco <christopher.franco.m@gmail.com>
2018-05-04 18:45:16 -0700

    added accordion functionality for team viewing

--- src/pages/about.js
+++ src/pages/about.js
@@ -137,35 +137,38 @@ export default class About extends React.Component {
                 </div>
                 <div className="container imagical-copy--styling">
                   <h1 className="title title--custom"> HOW WE DO IT</h1>
-                  <div className="container-circle-parent">
-                    <div className="container--sizing">
-                        <div className="container-circle--geo">
+                  <div className="container">
+                      <div className="row">
+                        <div className="col-sm-2">
+                            <button className="button container--sizing" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                              Strategy
+                            </button>
                         </div>
-                        <h3 className="container--category">Strategy</h3>
-                    </div>
-                    <div className="container--sizing">
-                        <div className="container-circle--geo">
+                        <div className="col-sm">
+                            <button className="button container--sizing" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Creative
+                            </button>
                         </div>
-                        <h3 className="container--category">Creative</h3>
-                    </div>
-                    <div className="container--sizing">
-                        <div className="container-circle--geo">
+                        <div className="col-sm mr-3">
+                          <button className="button container--sizing" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Public Relations
+                          </button>
                         </div>
-                        <h3 className="container--category">Public Relations</h3>
-                    </div>
-                    <div className="container--sizing">
-                        <div className="container-circle--geo">
+                        <div className="col-sm">
+                          <button className="button container--sizing" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            Media Planning
+                          </button>
                         </div>
-                        <h3 className="container--category">Media Planning</h3>
-                    </div>
-                    <div className="container--sizing">
-                        <div className="container-circle--geo">
+                        <div className="col-sm-2">
+                          <button className="button container--sizing" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            Production
+                          </button>
                         </div>
-                        <h3 className="container--category">Production</h3>
-                    </div>
-                  </div>
+                      </div>
                 </div>
-                <Team
+                <div className="collapse" id="collapseOne">
+                  <div class="card card-body">
+                  <Team
                   imgSrc={strategyphoto}
                   teamname ="Strategy"
                   description = "Gathers and analyzes the primary and secondary market research that is used to determine the campaign’s target market. This department is in charge of creating a single, unified theme and strategy that is applied to all aspects of the marketing campaign."
@@ -190,7 +193,12 @@ export default class About extends React.Component {
                     },
                   ]}
                   />
-                <Team
+                  </div>
+                </div>
+
+                <div className="collapse" id="collapseTwo">
+                  <div class="card card-body">
+                  <Team
                   imgSrc={creativephoto}
                   teamname ="Creative"
                   description = "Crafts, communicates, and guides execution of the campaign’s main idea. Tackles the challenge of articulating the brand or product’s unique value propositions in a way that is relevant, meaningful, and memorable to the consumer."
@@ -215,7 +223,12 @@ export default class About extends React.Component {
                     },
                   ]}
                   />
-                <Team
+                  </div>
+                </div>
+
+                <div className="collapse" id="collapseThree">
+                  <div class="card card-body">
+                  <Team
                   imgSrc={prphoto}
                   teamname ="Public Relations"
                   description = "Designs a cohesive public relations initiative that complements and extends the creative campaign by developing community programs, identifying strategic partnerships, and generating campaign awareness through organic media value."
@@ -234,7 +247,11 @@ export default class About extends React.Component {
                     },
                   ]}
                   />
-                <Team
+                  </div>
+                </div>
+                <div className="collapse" id="collapseFour">
+                  <div class="card card-body">
+                  <Team
                   imgSrc={mediaphoto}
                   teamname ="Media Planning"
                   description = "Develops a multi-million dollar purchasing plan by evaluating and researching the profitability of different media vehicles (i.e. TV, internet, social, etc.). This department creates the most effective strategy of placing ads through competitive benchmarking, consumer trend evaluations, and quantitative market analysis."
@@ -259,7 +276,11 @@ export default class About extends React.Component {
                     },
                   ]}
                   />
-                <Team
+                  </div>
+                </div>
+                <div className="collapse" id="collapseFive">
+                  <div class="card card-body">
+                  <Team
                   imgSrc={productionphoto}
                   teamname ="Production"
                   description = "Develops a multi-million dollar purchasing plan by evaluating and researching the profitability of different media vehicles (i.e. TV, internet, social, etc.). This department creates the most effective strategy of placing ads through competitive benchmarking, consumer trend evaluations, and quantitative market analysis."
@@ -281,6 +302,9 @@ export default class About extends React.Component {
                     },
                   ]}
                   />
+                  </div>
+                </div>
+              </div>
             </div>
         );
     }
