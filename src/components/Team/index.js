14aa335bf16a78bbc030bbe259adfbe73e946a55
Franco <christopher.franco.m@gmail.com>
2018-05-05 14:13:36 -0700

    about page completed next needs graphql integration

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -7,29 +7,31 @@ export default class Team extends React.Component {
       <li className="list-deliverables" key={key}>{deli.deliverable}</li>
       ));
         return (
-          <div className="container">
-            <div className="img--team-sizing">
-              <Image src={this.props.imgSrc} responsive />
-            </div>
-            <div className="row--resize">
-              <div className="col--custom">
-                <div className="col-team--start">
-                    <h2 className="column-title-team">{this.props.teamname}</h2>
-                    <p>{this.props.description}</p>
-                </div>
-                <a className="col-btn--flex button" href="/apply"> Apply</a>
+          <div className="container--team">
+            <div className="container">
+              <div className="img--team-sizing">
+                <Image src={this.props.imgSrc} responsive />
               </div>
-              <div className="col-lg-6">
-                <div>
-                  <h2 className="column-title-team">Key Deliverables</h2>
-                  <p>{this.props.description}</p>
-                  <div className="list-container">
-                    {deliverables}
+              <div className="row--resize">
+                <div className="col--custom">
+                  <div className="col-team--start">
+                      <h2 className="column-title-team">{this.props.teamname}</h2>
+                      <p>{this.props.description}</p>
+                  </div>
+                  <a className="col-btn--flex button" href="/apply"> Apply</a>
+                </div>
+                <div className="col-lg-6">
+                  <div>
+                    <h2 className="column-title-team">Key Deliverables</h2>
+                    <p>{this.props.description}</p>
+                    <div className="list-container">
+                      {deliverables}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+        </div>
         )
     }
 }
